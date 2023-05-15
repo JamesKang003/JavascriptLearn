afb8eb2a43da885a6ff381c664b19fdedd9ea538
JamesKang003 <kangjames5@gmail.com>
2023-05-15 21:44:27 +0900

    step 2 finished

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
-const tutorial = require('./tutorial');
+/**
+ * step 1)
+ * const tutorial = require('./tutorial');
 console.log(tutorial);
 console.log(tutorial.sum(1,1));
 console.log(tutorial.PI);
 console.log(new tutorial.SomeMathObj());
+ */
+
+/**
+ * step 2)
+ * 
+ * const EventEmitter = require("events");
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on("tutorial", (num1, num2) => {
+  console.log(num1 + num2);
+});
+
+eventEmitter.emit("tutorial", 1, 2);
+
+class Person extends EventEmitter {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+
+  get myName() {
+    return this.name;
+  }
+}
+
+let pedro = new Person("Pedro");
+let christina = new Person("Christina");
+christina.on("name", () => {
+  console.log("my name is " + christina.myName);
+});
+pedro.on("name", () => {
+  console.log("my name is " + pedro.myName);
+});
+
+pedro.emit("name");
+christina.emit("name");
+ */
--- tutorial.js
+++ tutorial.js
@@ -1,8 +1,8 @@
 const sum = (num1, num2) => num1 + num2;
 const PI = 3.14;
-class SomeMathObj{
-    constructor(){
-        console.log('object created');
-    }
+class SomeMathObj {
+  constructor() {
+    console.log("object created");
+  }
 }
-module.exports = {sum: sum, PI: PI, SomeMathObj: SomeMathObj};
+module.exports = { sum: sum, PI: PI, SomeMathObj: SomeMathObj };
