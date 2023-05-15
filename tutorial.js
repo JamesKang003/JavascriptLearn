8ddf7768795501cf573517ae8aaf5507f4ad1480
JamesKang003 <kangjames5@gmail.com>
2023-05-15 21:02:05 +0900

    checking

---
+++ tutorial.js
@@ -0,0 +1,10 @@
+const sum = (num1, num2) => num1 + num2;
+const PI = 3.14;
+class SomeMathObj{
+    constructor(){
+        console.log('object created');
+    }
+}
+module.exports.sum = sum;
+module.exports.PI = PI;
+module.exports.SomeMathObj = SomeMathObj;
