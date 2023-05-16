46ae780ef1eb6fe6f9676243fdb24903e54d5601
JamesKang003 <kangjames5@gmail.com>
2023-05-16 18:18:03 +0900

    step 6 finished

--- app.js
+++ app.js
@@ -149,3 +149,11 @@ r1.on("close", () => {
 //     }
 //   }
 // });
+
+//step 6
+// const fs = require("fs");
+// const readStream = fs.createReadStream("./example.txt", "utf8");
+// const writeStream = fs.createWriteStream("example2.txt");
+// readStream.on("data", (chunk) => {
+//   writeStream.write(chunk);
+// });
