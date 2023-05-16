2209aba2736bf5eb5a1d19646032bed915532f10
JamesKang003 <kangjames5@gmail.com>
2023-05-16 22:48:54 +0900

    step 13

--- app.js
+++ app.js
@@ -224,21 +224,23 @@ r1.on("close", () => {
 //   }
 // }
 
-const express = require("express");
-const app = express();
+// step 13
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+// app.use("/public", express.static(path.join(__dirname, "static")));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "static", "아이네.png"));
+// });
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+// app.get("/example", (req, res) => {
+//   res.send("hitting example route");
+// });
 
-app.get("/example", (req, res) => {
-  res.send("hitting example route");
-});
+// app.get("/example/:name/:age", (req, res) => {
+//   console.log(req.params);
+//   console.log(req.query);
+//   res.send(req.params.name + " : " + req.params.age);
+// });
 
-app.get("/example/:name/:age", (req, res) => {
-  console.log(req.params);
-  console.log(req.query);
-  res.send(req.params.name + " : " + req.params.age);
-});
-
-app.listen(3000);
+// app.listen(3000);
