e1465749f02ed6bfc5ca24190c2c07b6ad46de50
JamesKang003 <kangjames5@gmail.com>
2023-05-16 19:18:02 +0900

    step 7 completed

--- app.js
+++ app.js
@@ -157,3 +157,11 @@ r1.on("close", () => {
 // readStream.on("data", (chunk) => {
 //   writeStream.write(chunk);
 // });
+
+// step 7 completed
+// const fs = require("fs");
+// const zlib = require("zlib");
+// const gunzip = zlib.createGunzip();
+// const readStream = fs.createReadStream("./example2.txt.gz");
+// const writeStream = fs.createWriteStream("uc.txt");
+// readStream.pipe(gunzip).pipe(writeStream);
